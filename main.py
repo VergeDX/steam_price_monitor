f62f139e73323678e381515af3ac1ec7ed50ea9c
Vanilla <osu_Vanilla@126.com>
2022-03-21 14:44:08 +0800

    feat[types]: Remove Final, adding all types to var.

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import urllib.parse
-from typing import Final
 
 from game_model import SteamGame
 
@@ -7,12 +6,12 @@ OneShot: SteamGame = SteamGame("OneShot", 420530, 1800)
 Florence: SteamGame = SteamGame("Florence", 1102130, 1000)
 
 # https://stackoverflow.com/questions/2682745/how-do-i-create-a-constant-in-python
-monitoring_games: Final = [OneShot, Florence]
+monitoring_games: list[SteamGame] = [OneShot, Florence]
 
 # https://stackoverflow.com/questions/15799696/how-to-build-urls-in-python
-base_url: Final = "https://store.steampowered.com/api/appdetails?"
+base_url: str = "https://store.steampowered.com/api/appdetails?"
 
 if __name__ == '__main__':
     for game in monitoring_games:
-        params = {'appids': game.appId, 'cc': 'cn', 'filters': 'price_overview'}
-        print(base_url + urllib.parse.urlencode(params))
+        params: dict = {'appids': game.appId, 'cc': 'cn', 'filters': 'price_overview'}
+        url: str = base_url + urllib.parse.urlencode(params)
