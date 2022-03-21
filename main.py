8ed298121079d55c2dec94505e0d4920d45fe0bf
Vanilla <osu_Vanilla@126.com>
2022-03-21 14:21:22 +0800

    feat[main.py]: Making final appId_list, for it and pass.

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
+# https://stackoverflow.com/questions/2682745/how-do-i-create-a-constant-in-python
+from typing import Final
+
+# appName_list = ["OneShot", "Florence"]
+appId_list: Final = [420530, 1102130]
+
 if __name__ == '__main__':
-    pass
+    for appId in appId_list:
+        pass
