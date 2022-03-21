10fa27e9c0ba8353015dda7862c2122a5b8d72d4
Vanilla <osu_Vanilla@126.com>
2022-03-21 14:52:31 +0800

    fix[proxies]: https use http, due to local Qv2Ray.

--- proxy_chooser.py
+++ proxy_chooser.py
@@ -3,7 +3,7 @@ USE_PROXY: bool = True
 # https://www.scrapingbee.com/blog/python-requests-proxy/
 proxies = {
     'http': 'http://127.0.0.1:8889',
-    'https': 'https://127.0.0.1:8889',
+    'https': 'http://127.0.0.1:8889',
 }
 
 
