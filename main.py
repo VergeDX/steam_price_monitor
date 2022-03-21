337956e373f52302820ee4f815639a1033d268d2
Vanilla <osu_Vanilla@126.com>
2022-03-21 14:28:54 +0800

    feat[urllib3]: Using urllib.parse, making url.

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
+import urllib.parse
+
 # https://stackoverflow.com/questions/2682745/how-do-i-create-a-constant-in-python
 from typing import Final
 
 # appName_list = ["OneShot", "Florence"]
 appId_list: Final = [420530, 1102130]
 
+# https://stackoverflow.com/questions/15799696/how-to-build-urls-in-python
+base_url: Final = "https://store.steampowered.com/api/appdetails?"
+
 if __name__ == '__main__':
     for appId in appId_list:
-        pass
+        params = {'appids': appId, 'cc': 'cn', 'filters': 'price_overview'}
+        print(base_url + urllib.parse.urlencode(params))
