0a1c4232cafe4d5323a0a711278c3fd0598389af
Vanilla <osu_Vanilla@126.com>
2022-03-21 16:59:31 +0800

    style[main.py]: Move question mark from base_url to url.

--- main.py
+++ main.py
@@ -6,8 +6,7 @@ from grequests import AsyncRequest
 from game_list import monitoring_games
 
 # https://stackoverflow.com/questions/15799696/how-to-build-urls-in-python
-
-base_url: str = "https://store.steampowered.com/api/appdetails?"
+base_url: str = "https://store.steampowered.com/api/appdetails"
 
 if __name__ == '__main__':
     # https://stackoverflow.com/questions/9110593/asynchronous-requests-with-python-requests
@@ -15,7 +14,7 @@ if __name__ == '__main__':
 
     for game in monitoring_games:
         params: dict = {'appids': game.appId, 'cc': 'cn', 'filters': 'price_overview'}
-        url: str = base_url + urllib.parse.urlencode(params)
+        url: str = base_url + "?" + urllib.parse.urlencode(params)
 
         # https://github.com/spyoungtech/grequests
         action_item: AsyncRequest = grequests.get(url)
