18a03b770e65d50bd829e85fea740c0e41aad78b
Vanilla <osu_Vanilla@126.com>
2022-03-21 15:34:07 +0800

    style[main]: Adding type info AsyncRequest.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import urllib.parse
 
 import grequests
+from grequests import AsyncRequest
 
 from game_list import monitoring_games
 
@@ -10,14 +11,14 @@ base_url: str = "https://store.steampowered.com/api/appdetails?"
 
 if __name__ == '__main__':
     # https://stackoverflow.com/questions/9110593/asynchronous-requests-with-python-requests
-    async_list = []
+    async_list: list[AsyncRequest] = []
 
     for game in monitoring_games:
         params: dict = {'appids': game.appId, 'cc': 'cn', 'filters': 'price_overview'}
         url: str = base_url + urllib.parse.urlencode(params)
 
         # https://github.com/spyoungtech/grequests
-        action_item = grequests.get(url)
+        action_item: AsyncRequest = grequests.get(url)
         async_list.append(action_item)
 
     grequests.map(async_list)
