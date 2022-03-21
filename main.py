9f1c309865cd6bc43aac52aaef6c3c077cf5bc1c
Vanilla <osu_Vanilla@126.com>
2022-03-21 15:16:00 +0800

    feat[grequests]: Replace requests -> grequests.

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import urllib.parse
 
-import requests
-from requests import Response
-
 from game_list import monitoring_games
-from proxy_chooser import get_proxies
 
 # https://stackoverflow.com/questions/15799696/how-to-build-urls-in-python
 base_url: str = "https://store.steampowered.com/api/appdetails?"
@@ -13,6 +9,3 @@ if __name__ == '__main__':
     for game in monitoring_games:
         params: dict = {'appids': game.appId, 'cc': 'cn', 'filters': 'price_overview'}
         url: str = base_url + urllib.parse.urlencode(params)
-
-        # https://docs.python-requests.org/en/latest/
-        r: Response = requests.get(url, proxies=get_proxies())
